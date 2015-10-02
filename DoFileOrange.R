32cd3817ab982dafdee621cd304cb92498bf57d0
LarsMehwald <lars.mehwald@posteo.de>
2015-10-02 15:51:55 +0200

    Histogram

--- DoFileOrange.R
+++ DoFileOrange.R
@@ -5,6 +5,7 @@
 ##################################
 
 # Loading needed packages
+search()
 library('ggplot2')
 library('magrittr')
 
@@ -21,16 +22,34 @@ class(Orange) <- "data.frame"
 #Analysis of Orange dataset
 ##################################
 
-#Summary statistics: min-max, median, mean, 1st and 3rd quartile
+
+# Getting to know the data frame
+?Orange
+names(Orange)
+
+# Summary statistics: min-max, median, mean, 1st and 3rd quartile
 summary(Orange)
 
-#Standard deviation of 6 variables
+# Histogram for variables: age, circumference 
+hist(Orange$age, main = "Age of orange tree")
+hist(Orange$circumference, main = "Circumference of orange tree")
+
+# The histogram revealed that the variable age is skewed:
+# therefore the variable is transformed (log)
+Orange$age <- log(Orange$age)
+hist(Orange$age, main = "Age of orange tree")
+
+# Since this this transformation did not improve the distribution,
+# the original data is used in the following
+rm(Orange)
+data(Orange)
+
+# Standard deviation of 6 variables
 StandardDeviation(Orange)
 
-# Linear Regression: is the circumference of a tree a good 
-# predictor for its age?
+# Linear Regression: is the circumference a good predictor for age?
 OrangeLinearModel <- lm(age ~ circumference, data=Orange)
-# defining the linear model: age (y) as a function of circumference (x)
-# in the data frame Orange
+# defining the linear model: age as a function of circumference 
+# in the data.frame Orange
 summary(OrangeLinearModel) # output of results: statistical significant
 rm(OrangeLinearModel)
